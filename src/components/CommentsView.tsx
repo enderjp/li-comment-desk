7f42770538f4050103a24f0ea880cb6354176b40
Baudi <baudivergara@gmail.com>
2026-04-22 16:14:05 -0500

    filtro media_type visualmente integrado Abr22.3

--- src/components/CommentsView.tsx
+++ src/components/CommentsView.tsx
@@ -700,7 +700,7 @@ export function CommentsView({ prefilterAdset = '', selectedRequestId = '', ligh
               onChange={(e) => setSelectedMediaType(e.target.value)}
               className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white"
             >
-              <option value="all">Todos los tipos</option>
+              <option value="all">Todos los post</option>
               <option value="image">Imagen</option>
               <option value="video">Video</option>
             </select>
